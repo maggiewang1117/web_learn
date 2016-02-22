77a0c727c940810286a2ab31bffeb0997f6523a7
huiwa <huiwa@redhat.com>
2016-02-22 20:54:03 +0800

    learn chapter5

--- functional_test.py
+++ functional_test.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVistorTest(unittest.TestCase):
     def setUp(self):
@@ -12,6 +13,11 @@ class NewVistorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -24,13 +30,10 @@ class NewVistorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy paecock feathers') # input "Buy paecock feathers
 
-        inputbox.send_keys(keys.Enter) # press enter
-
+        inputbox.send_keys(Keys.ENTER) # press enter
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy paecock feathers' for row in rows)
-        )    # "1: Buy paecock feathers" in To-Do list now
+        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy paecock feathers')    # "1: Buy paecock feathers" in To-Do list now
 
         self.fail('Finish the test!')
 
--- lists/tests.py
+++ lists/tests.py
@@ -9,6 +9,8 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -17,8 +19,36 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest
+        request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expect_html = render_to_string('home.html',
+                                       {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expect_html)
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item, 'Item the second')
