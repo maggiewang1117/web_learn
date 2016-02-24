11cf17fbda0c088bb58726ffb426b1a4a205d25c
huiwa <huiwa@redhat.com>
2016-02-24 21:33:32 +0800

    learn in page 76

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -3,10 +3,10 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 import time
+from django.test import LiveServerTestCase
 
-class NewVistorTest(unittest.TestCase):
+class NewVistorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -20,7 +20,7 @@ class NewVistorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text) # header contains "To-Do"
@@ -37,6 +37,3 @@ class NewVistorTest(unittest.TestCase):
 
         self.fail('Finish the test!')
 
-
-if __name__ == '__main__':
-    unittest.main()
