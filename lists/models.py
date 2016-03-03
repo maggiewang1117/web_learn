b07ae2461f8c6042bad842567dc5d8e7d7f9dfe2
huiwa <huiwa@redhat.com>
2016-03-03 23:01:41 +0800

    finished chapter 6

--- lists/models.py
+++ lists/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
 
 class test(models.Model):
     test1 = models.CharField(max_length=100)
+
